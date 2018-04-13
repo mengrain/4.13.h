3bca1d8f145d1a4dcd03e72be442cb400a3fd5c5
mengrain <l>
2018-04-13 16:48:32 +0800

    Signed-off-by: mengrain <l>

--- app.js
+++ app.js
@@ -12,67 +12,50 @@ var pool = mysql.createPool({
 	database:'item',
 	port:3306
 }) 
-
-//查询
-app.post('/',(req,res) => {
-	res.setHeader('Access-Control-Allow-Origin','*')
-	pool.getConnection(function(err,connection){
-		if(err){
-			console.log(err)
-			return
-		}
-		var sql = `select * from moban where state=${req.body.state}`;
-		connection.query(sql,function(err,data){
-			if(err){
-				console.log(err)
-				return
-		    }
-			res.send(data)
-			connection.end()
-		})
-	})
+//读取
+app.use('/',(req,res) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    pool.getConnection(function(err,connection){
+        if(err){
+            console.log(err)
+            return
+        }
+        var sql = `select * from kecheng`
+        connection.query(sql,function(err,data){
+            if(err){
+                console.log(err)
+                return
+         } 
+             	res.send(data)
+             	connection.end()
+        })
+    })
 })
 
-//删除
-app.post('/del',(req,res) => {
-	res.setHeader('Access-Control-Allow-Origin','*')
-	pool.getConnection(function(err,connection){
-		if(err){
-			console.log(err)
-			return
-		}
-		var sql = `delete from moban where id=${req.body.id}`;
-		connection.query(sql,function(err,data){
-			if(err){
-				console.log(err)
-				return
-		    }
-			res.send(data)
-			connection.end()
-		})
-	})
+
+//修改
+app.use('/update',(req,res) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    pool.getConnection(function(err,connection){
+        if(err){
+            console.log(err)
+            return
+        }
+        var json = req.body
+        var sql = `update kecheng  set one=?,two=?,three=?,four=?,five=? where id=${json.lesson} `
+        connection.query(sql,[json.one,json.two,json.three,json.four,json.five],function(err,data){
+            if(err){
+                console.log(err)
+                return
+         } 
+             	res.send(data)
+             	connection.end()
+        })
+    })
 })
 
-//添加
-app.post('/add',(req,res) => {
-	res.setHeader('Access-Control-Allow-Origin','*')
-	pool.getConnection(function(err,connection){
-		if(err){
-			console.log(err)
-			return
-		}
-		var JSON=req.body;
-		var sql = `insert into moban(name,sex,state) values(?,?,?)`;
-		connection.query(sql,[JSON.name,JSON.sex,JSON.state],function(err,data){
-			if(err){
-				console.log(err)
-				return
-		    }
-			res.send(data)
-			connection.end()
-		})
-	})
-})
+
+
 
 app.listen(3000,function(){
 	console.log('ok')
